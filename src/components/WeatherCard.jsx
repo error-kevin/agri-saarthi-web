1e2141e8278c26d91c437478670c623917b7d940
Keshav Joshi <168259812+error-kevin@users.noreply.github.com>
2025-03-15 16:38:52 +0530

    soil feature add

---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Weather.css";
+
+
+const WeatherCard = ({ weatherData }) => {
+    if (!weatherData) return <p className="loading">Loading...</p>;
+
+    return (
+        <div className="weather-card">
+            <h3>{weatherData.city}</h3>
+            <p><strong>Temperature:</strong> {weatherData.temperature}°C</p>
+            <p><strong>Condition:</strong> {weatherData.description}</p>
+            <p><strong>Humidity:</strong> {weatherData.humidity}%</p>
+            <p><strong>Wind Speed:</strong> {weatherData.wind_speed} m/s</p>
+        </div>
+    );
+};
+
+export default WeatherCard;
