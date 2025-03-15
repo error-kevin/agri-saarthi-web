8ea55b421e6c878b68c1bc8ee42144a1507bdd86
Keshav Joshi <168259812+error-kevin@users.noreply.github.com>
2025-03-15 17:33:06 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import api from "./api";
@@ -5,6 +6,9 @@ import Weather from "./components/weather";
 import Auth from "./components/auth";
 import WeatherCard from "./components/WeatherCard";
 import SoilMoisture from "./components/SoilMoisture";
+import Home from "./components/Home";
+import Header from "./components/Header";
+
 
 api.get("/api/hello")
   .then(response => console.log(response.data))
@@ -61,8 +65,16 @@ function App() {
 
   return (
     <div>
+      <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<h2>About AgriSaarthi</h2>} />
+        <Route path="/contact" element={<h2>Contact Us</h2>} />
+      </Routes>
+    </Router>
       <h1>Agri-Saarthi</h1>
-      <Auth />
+      {/* <Auth /> */}
       <Weather />
       <SoilMoisture />
       <p>{message}</p>
